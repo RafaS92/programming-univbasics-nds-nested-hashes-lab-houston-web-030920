e76ca8f7c320a227fb7ccbf27a3571c421efff24
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-28 20:45:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,7 +24,14 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  return assembled_aoh.sort
+ array = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+    
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
